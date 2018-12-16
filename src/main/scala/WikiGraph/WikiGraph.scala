520cdd06b7af3f275426c858a972f8eb2fd2c65d
Eemeli Saari <eemeli.saari@student.tut.fi>
2018-12-16 20:01:43 +0200

    Started the general process

--- src/main/scala/WikiGraph/WikiGraph.scala
+++ src/main/scala/WikiGraph/WikiGraph.scala
@@ -2,7 +2,11 @@ package WikiGraph
 
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{Logger, Level}
+import org.apache.spark.rdd.RDD
 
+import Options._
+import Utils._
+import Parser._
 
 /**
  * Entrypoint for the software.
@@ -19,5 +23,17 @@ object WikiGraph extends App {
 
     val sc = spark.sparkContext
 
-    println("Hello world!")
+    import spark.implicits._
+
+    val opt = Options(args.toList).argv()
+
+    println(opt("input_path"))
+    val paths : RDD[String] = sc.parallelize(listdir(opt("input_path")))
+    //paths.foreach(println)
+    val raw = spark.read.textFile("data/articles/291.txt")
+    println(raw.reduce(_ + _))
+
+    val conn : RDD[(String, Set[String])] = paths
+        .map(p => (p, findConnections(spark.read.textFile(p).reduce(_ + _))))
+    //conn.collect().foreach{case (name, set) => println(set)}
 }
