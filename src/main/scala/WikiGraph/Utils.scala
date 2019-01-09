962803e5544c64f182bb88073b4204cd33078a56
Eemeli Saari <eemeli.saari@student.tut.fi>
2019-01-09 21:37:47 +0200

    Refactored the utility functions

--- src/main/scala/WikiGraph/Utils.scala
+++ src/main/scala/WikiGraph/Utils.scala
@@ -4,25 +4,22 @@ import java.io._
 import org.apache.spark.sql.{DataFrame, Row}
 
 
-
 object Utils {
     def listDir(path: String) : Array[String] = {
         return new File(path).listFiles.map(f => f.getPath())
     }
     def fileName(path: String) : String = {
-        return path.tail.split("/").last
+        return path.tail.split("/").last.split("\\.")(0)
     }
     def df2csv(df:DataFrame, filename:String, delimiter:String = ",") : Unit = {
 
-        val rows = df.collect().map { 
-            case (x) => x.mkString(delimiter) + "\n" 
-        }
+        val rows = df.rdd.map(_.toSeq.mkString(delimiter) + "\n")
 
         val file = new BufferedWriter(new FileWriter(filename))
         val w = new PrintWriter(file)
 
         w.write(df.columns.mkString(delimiter) + "\n")
-        rows.foreach(r => w.write(r))
+        rows.collect.map(r => w.write(r))
         w.close
     }
     def dfShape(df:DataFrame) : Unit = {
