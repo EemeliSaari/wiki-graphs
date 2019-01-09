2f6fe31e2588abe1b32248709751377315df1b3b
Eemeli Saari <saari.eemeli@gmail.com>
2019-01-09 15:28:12 +0200

    Small df2csv function

--- src/main/scala/WikiGraph/Utils.scala
+++ src/main/scala/WikiGraph/Utils.scala
@@ -1,6 +1,8 @@
 package WikiGraph
 
-import java.io.File
+import java.io._
+import org.apache.spark.sql.{DataFrame, Row}
+
 
 
 object Utils {
@@ -9,5 +11,23 @@ object Utils {
     }
     def fileName(path: String) : String = {
         return path.tail.split("/").last
+    }
+    def df2csv(df:DataFrame, filename:String, delimiter:String = ",") : Unit = {
+
+        val rows = df.collect().map { 
+            case (x) => x.mkString(delimiter) + "\n" 
+        }
+
+        val file = new BufferedWriter(new FileWriter(filename))
+        val w = new PrintWriter(file)
+
+        w.write(df.columns.mkString(delimiter) + "\n")
+        rows.foreach(r => w.write(r))
+        w.close
+    }
+    def dfShape(df:DataFrame) : Unit = {
+        val rows = df.count()
+        val cols = df.columns.size
+        println("(" + rows + ", " + cols + ")")
     }
 }
