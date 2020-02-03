0e99dddccc1371f022b5017686bb54fa593d6db6
Eemeli Saari <saari.eemeli@gmail.com>
2020-02-03 20:17:46 +0200

    Refactor the main program to output the edge list

--- src/main/scala/WikiGraph/WikiGraph.scala
+++ src/main/scala/WikiGraph/WikiGraph.scala
@@ -10,7 +10,30 @@ import org.apache.spark.sql.functions.col
 import Options._
 import Utils._
 import Parser._
-import Connection._
+//import UserGraph._
+
+
+import org.apache.spark.sql.{DataFrame, Row}
+//import org.apache.spark.rdd.RDD
+import org.apache.spark.graphx.{Graph, Edge, VertexId}
+
+object GraphLoader {
+    val defaultConnection = ("Missing")
+
+    def initializeGraph(edges:DataFrame, nodes:DataFrame) : Graph[String, String] = {
+        return Graph(parseNodes(nodes), parseEdges(edges), defaultConnection)
+    }
+
+    def parseEdges(edges:DataFrame) : RDD[Edge[String]] = {
+        return edges.rdd
+            .map{case Row(src: Int, dst:Int) => Edge(src, dst, "reference")}
+    }
+
+    def parseNodes(nodes:DataFrame) : RDD[(VertexId, (String))] = {
+        return nodes.rdd
+            .map{case Row(id: Int, property:String) => (id, (property))}
+    }
+}
 
 /**
  * Entrypoint for the software.
@@ -40,15 +63,42 @@ object WikiGraph extends App {
         .as("nodes")
 
     nodes.show()
-    val paths = sc.parallelize(listDir(opt("input_path"))).reduce(_ + "," + _)
-    val raw = sc.wholeTextFiles(paths)
 
+    val t2 = System.nanoTime
+
+    val inputPath = opt("input_path")
+    var paths : RDD[String] = sc.emptyRDD
+    //if(isCsv(inputPath))
+    val df = spark.read
+        .format("csv")
+        .option("header", "true")
+        .option("delimiter", ";")
+        .load(inputPath)
+        .select("abspath")
+    println((System.nanoTime - t2)/1e9d)
+
+    df.show()
+
+    paths = df
+        .rdd
+        .map{case Row(value: String) => value}
+    //else
+        //paths = sc.parallelize(listDir(inputPath))
+
+    println("Readed all the paths!")
+
+    println("Loaded the files...")
     val t1 = System.nanoTime
 
-    val parsed = raw
-        .map(x => (fileName(x._1), findConnections(x._2)))
+    //paths.collect().foreach(println)
+
+    val parsed = paths
+        .map(x => (parseFilename(x), connectionsFromFile(x)))
+
+    println("Parsing step 1. Done!")
 
     val duration = (System.nanoTime - t1) / 1e9d
+    println(duration)
 
     val edges = parsed
         .flatMap(pair => pair._2.map(value => (pair._1, value)))
@@ -59,11 +109,11 @@ object WikiGraph extends App {
         .withColumnRenamed("Id", "DstId")
         .select("SrcId", "DstId")
 
-    val graph = initializeGraph(nodes, edges)
+    println("Acquired edges")
+
+    //val graph = GraphLoader.initializeGraph(nodes, edges)
 
     df2csv(edges, "edges.csv")
 
-    println(duration)
-    edges.show()
     dfShape(edges)
 }
